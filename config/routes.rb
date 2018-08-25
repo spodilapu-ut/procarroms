80189a1c5be96543e9686a50801591f77ec0c3d5
abhishek_sidabs <abhishek@sidabs.com>
2018-08-25 12:14:50 +0530

    Changed the root page route to Matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :matches do
-    resources :scores
-  end
-  resources :teams do
-      resources :members
-  end
 
-  resources :users
+  	root 'matches#index'
+  	resources :matches do
+    	resources :scores
+  	end
+  	resources :teams do
+    	resources :members
+  	end
+
+  	resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
