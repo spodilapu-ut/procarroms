bc246b452690e0731ba7ac98935345b79bdbc7f5
abhishek_sidabs <abhishek@sidabs.com>
2018-08-27 16:12:12 +0530

    Changed Dropdown to Radio button in New Scores

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,5 @@ class Team < ApplicationRecord
   has_many :team_twos, :class_name => 'Match', :foreign_key => 'team_two_id'
   has_many :tosses, :class_name => 'Match', :foreign_key => 'toss_id'
   has_many :members
+  has_many :scores
 end
